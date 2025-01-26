0083141d664a928ccd86b56a81f2c674d0437722
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-01-26 18:41:26 +0200

    Created If statement to check for YOLO and +++. replaced the entire screen display with H1 tag.  Also added console error message and console track to call stack

--- scripts.js
+++ scripts.js
@@ -17,6 +17,17 @@ form.addEventListener("submit", (event) => {
     alert("Division not performed. Invalid number provided. Try again.");
     return;
   }
+ // Check for "YOLO" and  "+++"
+  if (dividend === "YOLO" && divider === "+++"){
+    // Replace entire screen and display message
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
+    //Log the error  
+    console.error ("Critical error: Invalid input detected");
+    //logs the call stack 
+    console.trace();
+   return;
+
+  }
   // added Math.trunc()
   result.innerText = Math.trunc(dividend / divider);
 });
