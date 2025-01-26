e2b02261694cf5b612669c7ac717d8ed964fe923
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-01-26 16:59:02 +0200

    Added If statements. First one was to check if  Dividend or Divider is false. The second If statement was if Divider is equals to zero

--- scripts.js
+++ scripts.js
@@ -5,5 +5,18 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (!dividend || !divider) {
+    alert(
+      "Division not performed. Both values are required in inputs. Try again."
+    );
+    return; // Prevent division if inputs are empty
+  }
+
+  //Check if divider is equal to zero 
+  if (divider == 0) {
+    alert("Division not performed. Invalid number provided. Try again.");
+    return;
+  }
+  // added Math.trunc()
   result.innerText = Math.trunc(dividend / divider);
 });
